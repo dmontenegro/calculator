bdab90f0e645431a185da5045c00a37ec5d1c0b2
Juan Pablo Giron <jpgiron@localhost.localdomain>
2012-09-20 18:51:55 -0500

    Se modifico el Main

--- Main.sh
+++ Main.sh
@@ -1,50 +1,35 @@
-#!/bin/bash
-function Main(){
-exec 3>&1
-par1=$(dialog --title "Calculadora Interactiva" \
- --backtitle "Linux Para Ingenieria" \
- --inputbox "Digite primer numero" 8 50 \
-2>&1 1>&3)
+#!/bin/sh
 
-par2=$(dialog --title "Calculadora Interactiva" \
- --backtitle "Linux Para Ingenieria" \
- --inputbox "Digite simbolo" 8 50 \
-2>&1 1>&3)
+x=$(zenity --entry \
+	--title="Calculadora Interactiva" \
+	--text="Digite el primer número" \
+	--entry-text "Su número")
+	
+y=$(zenity --entry \
+	--title="Calculadora Interactiva" \
+	--text="Digite el operador" \
+	--entry-text "Su número")
 
-par3=$(dialog --title "Calculadora Interactiva" \
- --backtitle "Linux Para Ingenieria" \
- --inputbox "Digite segundo numero " 8 50 \
-2>&1 1>&3)
+z=$(zenity --entry \
+	--title="Calculadora Interactiva" \
+	--text="Digite el segundo número" \
+	--entry-text "Su número")
 
- case $par2 in
+case $y in
   "+")
-    echo "Se suma " $par1 " con " $par3
-    echo "El resultado es: "
-    ANSWER=$(source Suma.sh $par1 $par3)
+    ANSWER=$(source Suma.sh $x $z)
     ;;
   "-")
-    echo "Se resta" $par1 "con" $par3
-    echo "El resultado es : "
-    ANSWER=$(source Resta.sh $par1 $par3)
+    ANSWER=$(source Resta.sh $x $z)
   ;;
   "x")
-    echo "Se multiplica" $par1 " con " $par3
-    echo " el resultado es: "
-    ANSWER=$(source mult.sh $par1 $par3)
+    ANSWER=$(source mult.sh $x $z)
   ;;
   "/")
-    echo "Se divide" $par1 "con" $par3
-    echo "el resultado es: "
-    ANSWER=$(source division.sh $par1 $par3)
+    ANSWER=$(source division.sh $x $z)
   ;;
  esac
 
-dialog --title "Calculadora Interactiva" \
- --backtitle "Linux Para Ingenieria" \
- --infobox "$ANSWER" 3 34
-
-
-}
- 
-
-Main
+zenity ---info \
+	--title="Resultado" \
+	--text="$ANSWER"
